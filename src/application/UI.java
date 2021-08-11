0c9077153046351b11fd50d66f0c455aff9d0b87
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-11 09:08:26 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -66,4 +66,10 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
+	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+	 System.out.print("\033[H\033[2J");
+	 System.out.flush();
+	} 
 }
